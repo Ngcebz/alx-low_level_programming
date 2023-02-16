f143db4f45341e821307912c3f9a6a77b9a1d3c8
Nongcebo <nssbmcnamarabold@yahoo.com>
2023-02-16 12:12:37 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,7 @@
  * main - A program that prints a line with puts function
  * Return: 0 (Success)
  */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzz
-le");
+int main(void){
+puts("\n"Programming is like building a multilingual puzzle");
 return (0);
 }
