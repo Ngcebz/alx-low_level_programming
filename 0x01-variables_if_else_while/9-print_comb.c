31d4b6d160d7ac750b40c70101182f7f33bac087
Nongcebo <nssbmcnamarabold@yahoo.com>
2023-02-20 08:38:12 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - main block
+ * main - main function
  * Description: prints all single number of base 10
  * starting 0, follwoed by a new line
- * Return 0
+ * Return: 0
  */
 int main(void)
 {
